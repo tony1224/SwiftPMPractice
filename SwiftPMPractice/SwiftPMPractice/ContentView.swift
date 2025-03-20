ff6cf9e532cfa4bb04454a0a795197e6f9682927
tony1224 <starmine2013@gmail.com>
2025-03-20 23:30:40 +0900

    create first commit

---
+++ SwiftPMPractice/SwiftPMPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPMPractice
+//
+//  Created by Jun Morita on 2025/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPMPractice/SwiftPMPractice/SwiftPMPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPMPracticeApp.swift
+//  SwiftPMPractice
+//
+//  Created by Jun Morita on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPMPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
