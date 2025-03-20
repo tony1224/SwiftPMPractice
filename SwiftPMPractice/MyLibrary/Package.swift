3998cff567735ee36b9191f66b80c3bb1710fa4a
tony1224 <starmine2013@gmail.com>
2025-03-21 00:06:27 +0900

    add dependencies

--- SwiftPMPractice/MyLibrary/Package.swift
+++ SwiftPMPractice/MyLibrary/Package.swift
@@ -14,11 +14,12 @@ let package = Package(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/YusukeHosonuma/SwiftPrettyPrint.git", from: "1.4.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyLibrary"),
-
+        .target(name: "MyLibrary", dependencies: ["SwiftPrettyPrint"]),
     ]
 )
